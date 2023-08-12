c9eb718faa4c988f01b849459a96e4bccb12baa8
franklinfilion <franklinfilion@gmail.com>
2023-08-11 20:10:22 -0400

    inicio

---
+++ Datatype.py
@@ -0,0 +1,19 @@
+# Fundamental Data type {int, float}
+# eval() and type() functions
+#Operations
+print(6 - 3)
+print(2 + 1)
+print(2 * 4)
+print("float division", 10 / 3)  # float division
+print("int division", 10 // 3)
+print("printing a string: " + "Hi")
+
+# uso de eval
+print("uso de eval() -> eval(\"4 + 3\") = ", eval("4 + 3"))
+
+print(type(eval("4 + 3")))
+print(type(eval("4.0 + 3")))
+print(type(["apple", "banana", "cherry"]))
+print(type({"apple", "banana", "cherry"}))
+print(type(("apple", "banana", "cherry")))
+print(type({1: 'One', 2: 'Two'}))
---
+++ input.py
@@ -0,0 +1,6 @@
+#Interpreter
+#Resource: Python.org, Repl.it, Glot.io
+#Using input() function
+
+name = input('What is your name ? ')
+print('Hello ' + name)
